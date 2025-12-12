9bce82dcbc7078528ed57622dc6b11153b8f4df6
ezgiaktas <ezgiaktas@localhost>
2025-12-12 15:42:50 +0300

    opened product class and its constructor

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,25 @@
 
 public class Product {
+	 private String productID;
+	 private String name;
+	 private double price;
+	 private int quantity;
+
+
+
+	 public Product(String productID, String name, double price, int quantity) {
+	        this.productID = productID;
+	        this.name = name;
+	        this.price = price;
+	        this.quantity = quantity;
+	    }
+
+
+
+
+
+
+
+
 
 }
