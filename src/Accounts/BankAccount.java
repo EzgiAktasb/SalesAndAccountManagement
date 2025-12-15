187fe31c9878a2c49770cc91b98c9af4e758f80f
12Ceyda <ceylingulerler@gmail.com>
2025-12-16 00:40:12 +0300

    BankAccount.java

--- src/Accounts/BankAccount.java
+++ src/Accounts/BankAccount.java
@@ -1,10 +1,30 @@
 package Accounts;
 
-public class BankAccount {
+import model.Account;
 
-	
-	
-	
-	
-	
+public class BankAccount extends Account {
+
+    private String bankName;
+    private String iban;
+
+    public BankAccount(String accountID, double balance, String bankName, String iban) {
+        super(accountID);
+        this.balance = balance;
+        this.bankName = bankName;
+        this.iban = iban;
+    }
+  
+    
+    public void updateBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public void transferFunds(BankAccount target, double amount) {
+        if (amount <= balance) {
+            this.balance -= amount;
+            target.updateBalance(amount);
+        } else {
+            System.out.println("Insufficient balance for transfer.");
+        }
+    }
 }
