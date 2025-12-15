a4a03cabce761a7f89e343fe9ee70f952c03d9cd
12Ceyda <ceylingulerler@gmail.com>
2025-12-16 00:40:50 +0300

    CashAccount.java

--- src/Accounts/CashAccount.java
+++ src/Accounts/CashAccount.java
@@ -1,5 +1,27 @@
 package Accounts;
 
-public class CashAccount {
+import model.Account;
 
+public class CashAccount extends Account {
+
+    public CashAccount(String accountID, double balance) {
+        super(accountID);
+        this.balance = balance;
+    }
+ 
+    
+    public void updateBalance(double amount) {
+        balance += amount;
+    }
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient cash balance.");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
 }
