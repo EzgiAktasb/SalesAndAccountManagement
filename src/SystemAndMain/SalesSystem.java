464f0c688316b7e76a56d1574f85ea658b0b4e57
AhmetKrmc <can01business@gmail.com>
2025-12-16 16:25:23 +0300

    Edit8

--- src/SystemAndMain/SalesSystem.java
+++ src/SystemAndMain/SalesSystem.java
@@ -9,9 +9,9 @@ import java.util.Set;
 
 public class SalesSystem {
 
-    private ArrayList<Product> product;
-    private Set<Transaction> transactions;
-    private Map<String, Account> accounts;
+    private static ArrayList<Product> product;
+    private static Set<Transaction> transactions;
+    private static Map<String, Account> accounts;
 
     public SalesSystem() {
         product = new ArrayList<>();
@@ -19,38 +19,38 @@ public class SalesSystem {
         accounts = new HashMap<>();
     }
 
-    public void addProduct(Product p) {
+    public static void addProduct(Product p) {
         product.add(p);
     }
 
-    public void addTransaction(Transaction t) {
+    public static void addTransaction(Transaction t) {
         transactions.add(t);
         t.applyTransaction();
     }
 
-    public void addAccount(Account a) {
+    public static void addAccount(Account a) {
         accounts.put(a.getAccountID(), a);
     }
 
-    public void displayAllProducts() {
+    public static void displayAllProducts() {
         for (Product p : product) {
             p.displayProduct();
         }
     }
 
-    public void displayAllTransactions() {
+    public static void displayAllTransactions() {
         for (Transaction t : transactions) {
             t.displayTransaction();
         }
     }
 
-    public void displayAllAccounts() {
+    public static void displayAllAccounts() {
         for (Account a : accounts.values()) {
             a.displayAccountInfo();
         }
     }
 
-    public Product searchProductById(String id) {
+    public static Product searchProductById(String id) {
         for (Product p : product) {
             if (p.getProductID().equals(id)) {
                 return p;
@@ -59,19 +59,19 @@ public class SalesSystem {
         return null;
     }
 
-    public Account searchAccountById(String id) {
+    public static Account searchAccountById(String id) {
         return accounts.get(id);
     }
 
-    public boolean deleteProduct(String id) {
+    public static boolean deleteProduct(String id) {
         return product.removeIf(p -> p.getProductId().equals(id));
     }
 
-    public boolean deleteAccount(String id) {
+    public static boolean deleteAccount(String id) {
         return accounts.remove(id) != null;
     }
 
-    public double calculateTotalRevenue() {
+    public static double calculateTotalRevenue() {
         double total = 0.0;
         for (Transaction t : transactions) {
             total += t.calculateTransactionAmount();
@@ -79,7 +79,7 @@ public class SalesSystem {
         return total;
     }
 
-    public void updateAccountBalance(String id, double amount) {
+    public static void updateAccountBalance(String id, double amount) {
         Account account = accounts.get(id);
         if (account != null) {
             account.updateBalance(amount);
